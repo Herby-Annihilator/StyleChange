1a6cd499f83e8f7b099b06d9c1774064dc1df2f2
Danil Rukin <Rukin2018D@yandex.ru>
2020-02-29 12:38:32 +0700

    Использование сторонних dll

--- Program.cs
+++ Program.cs
@@ -7,18 +7,22 @@ using Microsoft.Win32;
 using System.Activities;
 using System.Configuration;
 using System.Windows.Forms;
+using System.Drawing.Design;
+using System.Runtime.InteropServices;
 
 namespace StyleChange
 {
     class Program
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
         static void Main(string[] args)
         {
-
-           
-
-
-            RegistryKey key = Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop", true);
+            //RegistryKey key = Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop", true);
             RegistryKey registry = Registry.CurrentUser.OpenSubKey(@"Control Panel\\Desktop", true);
             string[] str = registry.GetValueNames();
             for (int i = 0; i < str.Length; i++)
@@ -26,9 +30,19 @@ namespace StyleChange
                 Console.WriteLine(str[i]);
             }
 
-            registry.SetValue("Wallpaper", "C:\\Users\\User\\Desktop\\Учебники\\1 курс\\Officia_l_Windows_10.jpg");
-            registry.SetValue("PrevWallpaper", "C:\\Users\\User\\Desktop\\Учебники\\1 курс\\Officia_l_Windows_10.jpg");
+            //registry.SetValue("Wallpaper", "C:\\Users\\Данил\\Рабочий стол темы\\Светлая\\2.bmp");
+            //registry.SetValue("PrevWallpaper", "C:\\Users\\Данил\\Рабочий стол темы\\Светлая\\2.bmp");
+
+
+           // registry.SetValue("ButtonFace", "255 255 255"); // prev = 212 208 200
             registry.Close();
+
+            
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+            0,
+            "C:\\Users\\Данил\\Рабочий стол темы\\Светлая\\1.bmp",
+            SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+
             
             Console.ReadKey(true);
         }
