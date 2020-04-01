4aa055c94d075b8f75fcfbdb2a8b05bdef309248
Danil Rukin <Rukin2018D@yandex.ru>
2020-04-01 11:09:30 +0700

    начни уже что-нибудь делать

---
+++ Controllers/Executors/RegistryExecutor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace StyleChange.Controllers.Executors
+{
+    public class RegistryExecutor : Executor
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Registry Executor";
+            }
+        }
+
+        //
+        // проверить, существует ли запись в реестре, причем проверить надо все (рекурсивно)
+        //
+    }
+}
